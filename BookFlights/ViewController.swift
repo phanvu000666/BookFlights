ad07376a5135937bfb2a09dd859357f2d802c3b6
Khả Như <khanhu@khas-mac.local>
2022-05-11 06:55:30 -0700

    Initial Commit

---
+++ BookFlights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookFlights
+//
+//  Created by Khả Như on 5/11/22.
+//  Copyright © 2022 hoangvu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
